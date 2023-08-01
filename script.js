4f7e4b2ac5ff1767069082a255be093f99d6f457
Asif-4747 <asifhameed426@gmail.com>
2023-08-01 15:47:28 +0530

    demo

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function message(){
-    alert("MOHAMED ASIF 5 1911086 A");
+    var x1=document.getElementById("name").value;
+    var x2=document.getElementById("class").value;
+    var x3=document.getElementById("regno").value;
+    var x4=document.getElementById("grade").value;
+    
+    alert("Name : "+x1+"\n class :"+x2+"\n regno :"+x3+"\n grade :"+x4);
+
 }
