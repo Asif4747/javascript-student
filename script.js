58fc5a6dd5e8b3c70d0397fff80eb35b1373a2ce
Asif-4747 <asifhameed426@gmail.com>
2023-08-03 10:55:34 +0530

    demo

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-function message(){
-    var x1=document.getElementById("name").value;
-    var x2=document.getElementById("class").value;
-    var x3=document.getElementById("regno").value;
-    var x4=document.getElementById("grade").value;
-    
-    let result = document.querySelector("#result")
-    result.innerHTML = "Name : "+x1+" class : "+x2+" regno : "+x3+" grade : "+x4
+var Sname = [];
+var Sclass = [];
+var Sreg = [];
+var Sgrade = [];
 
+var n = 1;
+var x = 0;
+
+function AddRow(){
+ 
+    var AddRown = document.getElementById('table');
+	var NewRow = AddRown.insertRow(n);
     
-}  
+
+    Sname[x] = document.getElementById("name").value;
+    Sclass[x] = document.getElementById("class").value;
+    Sreg[x] = document.getElementById("regno").value;
+    Sgrade[x] = document.getElementById("grade").value;
+
+    var cel1 = NewRow.insertCell(0);
+    var cel2 = NewRow.insertCell(1);
+    var cel3 = NewRow.insertCell(2);
+    var cel4 = NewRow.insertCell(3);
+
+    cel1.innerHTML = Sname[x];
+    cel2.innerHTML = Sclass[x];
+    cel3.innerHTML = Sreg[x];
+    cel4.innerHTML = Sgrade[x];
+
+    n++;
+    x++;
+}
